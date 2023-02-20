a4b5098ad2a3c0ea3c6ac6b08b0025ce45f2b759
Parvathyrmenon <110888637+Parvathyrmenon@users.noreply.github.com>
2023-02-20 15:20:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title("My Parents' new healthy diner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry oatmeal")
-streamlit.text("Kale, spinach and rocket smoothie")
-streamlit.text("Hard boiled free range egg")
+streamlit.text("🥣Omega 3 & Blueberry oatmeal")
+streamlit.text("🥗Kale, spinach and rocket smoothie")
+streamlit.text("🐔Hard boiled free range egg")
+streamlit.text("🥑🍞Avocado toast")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
